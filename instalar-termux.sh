e86a60f527e70faff16c7c8642a8710afdd6889a
Kato <40585523+xKatooo@users.noreply.github.com>
2020-06-03 10:08:54 -0500

    Actualización 2.1
    Ahora el programa se puede hacer path

--- instalar-termux.sh
+++ instalar-termux.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 echo 'instalando [+] >>>'
 echo 'creando permisos [+] >>'
 chmod 777 ngrok
 echo 'cambiando datos a directorio raiz [+] >>>'
 cp ngrok $HOME 
+read -p "Desea que el programa se haga path? s/n: " option
+if [ "$option" == "s" or "$option" == "S" ];
+then
+        echo 'Haciendo path el programa [+] >>>'
+        cp ngrok ../usr/bin/
+else
+        echo 'okay xd'
+fi
 cp LICENSE $HOME
 echo ''
 echo 'proceso completado, tkm <3 [+] >>>'
