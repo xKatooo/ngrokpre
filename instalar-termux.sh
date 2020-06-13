eac19bf6f412873f6ec4a56c7a9ce37d5aa48dd0
Kato <40585523+xKatooo@users.noreply.github.com>
2020-06-13 08:23:49 -0500

    Up 2.3

--- instalar-termux.sh
+++ instalar-termux.sh
@@ -5,11 +5,12 @@ chmod 777 ngrok
 echo 'cambiando datos a directorio raiz [+] >>>'
 cp ngrok $HOME 
 read -p "Desea que el programa se haga path? s/n: " option
-if ["$option" == "s"];then
-        echo 'Instalando el programa en el path [+] >>>'
-        cp ngrok ../usr/bin/
+if ["$option" == 's']
+then
+    echo 'Instalando el programa en el path [+] >>>'
+    cp ngrok ../usr/bin/
 else
-        echo 'okay xd'
+    echo 'okay xd'
 fi
 cp LICENSE $HOME
 cd $HOME
