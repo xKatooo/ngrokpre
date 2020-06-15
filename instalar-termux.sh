2ae8cdfe840328f92c5bfbae8ff6dcc50503eb9d
Kato <40585523+xKatooo@users.noreply.github.com>
2020-06-15 00:00:52 -0500

    Update 3.0
    Ahora se instala solo :)

--- instalar-termux.sh
+++ instalar-termux.sh
@@ -4,14 +4,8 @@ echo 'creando permisos [+] >>'
 chmod 777 ngrok
 echo 'cambiando datos a directorio raiz [+] >>>'
 cp ngrok $HOME 
-read -p "Desea que el programa se haga path? s/n: " option
-if [$option == 's']
-then
-    echo 'Instalando el programa en el path [+] >>>'
-    cp ngrok ../usr/bin/
-else
-    echo 'okay xd'
-fi
+echo 'Instalando el programa en el path [+] >>>'
+cp ngrok ../usr/bin/
 cp LICENSE $HOME
 cd $HOME
 rm -rf ngrokpre
